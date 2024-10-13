ab9e038f2c7df2970a0ff65c8b0e04f84bc7d693
ajujacob <ajujacob88@gmail.com>
2024-10-13 11:17:45 +0530

    first graph completed and making the page scrollable

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -11,74 +11,80 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Title bar
-          const TitleBar(),
-          // Section with two side-by-side graphs
-          Expanded(
-            flex: 6,
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    // margin: EdgeInsets.all(8.0),
-                    margin: const EdgeInsets.fromLTRB(28, 8, 8, 8),
-                    color: Colors.white,
-                    child: Center(child: GraphOne()),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(8, 8, 28, 8),
-                    color: Colors.white,
-                    child: const Center(child: Text('Graph 2')),
-                  ),
-                ),
-              ],
-            ),
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title bar
+            const TitleBar(),
+            // Section with two side-by-side graphs
+            SizedBox(
+              //flex: 6,
+              height: 473, // Fixed height for the graph section
 
-          // Small graphs section
-          Container(
-            margin: const EdgeInsets.fromLTRB(28, 8, 28, 8),
-            color: Colors.white,
-            height: 100, // Adjust the height for smaller graphs
-            child: const Center(child: Text('Small Graphs Section')),
-          ),
-
-          // Bottom section with three graphs side by side
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(28, 8, 8, 8),
-                    color: Colors.white,
-                    child: const Center(child: Text('Graph 3')),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      // margin: EdgeInsets.all(8.0),
+                      margin: const EdgeInsets.fromLTRB(28, 8, 8, 8),
+                      color: Colors.white,
+                      child: Center(child: GraphOne()),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(8.0),
-                    color: Colors.white,
-                    child: Center(child: Text('Graph 4')),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.fromLTRB(8, 8, 28, 8),
+                      color: Colors.white,
+                      child: const Center(child: Text('Graph 2')),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(8, 8, 28, 8),
-                    color: Colors.white,
-                    child: const Center(child: Text('Graph 5')),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+
+            // Small graphs section
+            Container(
+              margin: const EdgeInsets.fromLTRB(28, 8, 28, 8),
+              color: Colors.white,
+              height: 100, // Adjust the height for smaller graphs
+              child: const Center(child: Text('Small Graphs Section')),
+            ),
+
+            // Bottom section with three graphs side by side
+            SizedBox(
+              height: 300, // Fixed height for the graph section
+
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.fromLTRB(28, 8, 8, 8),
+                      color: Colors.white,
+                      child: const Center(child: Text('Graph 3')),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8.0),
+                      color: Colors.white,
+                      child: Center(child: Text('Graph 4')),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.fromLTRB(8, 8, 28, 8),
+                      color: Colors.white,
+                      child: const Center(child: Text('Graph 5')),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
